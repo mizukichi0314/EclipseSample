bc4faeb5e2d28c2dd9d7a8456206917e0bcfdc33
m.hashiba <118049319+mizukichi0314@users.noreply.github.com>
2023-10-16 20:30:51 +0900

    first commit

---
+++ exercise/src/exercise/first.java
@@ -0,0 +1,13 @@
+package exercise;
+
+public class first {
+
+	public static void main(String[] args) {
+		System.out.println("Eclipseを始めよう！");
+		System.out.println(20+23);
+		System.out.println(10/5);
+		System.out.println(20%8);
+		System.out.println(2+(5*10));
+	}
+
+}
