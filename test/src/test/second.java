fb16ffb9bd68f068a95461e994d5c9ad1c50893f
m.hashiba <118049319+mizukichi0314@users.noreply.github.com>
2023-10-25 23:16:02 +0900

    変数の代入と計算

--- test/src/test/second.java
+++ test/src/test/second.java
@@ -3,7 +3,7 @@ package test;
 public class second {
 
 	public static void main(String[] args) {
-//		課題１
+		// 課題１
 		int x = 7;
 		x = x * 3;
 		System.out.println(x);
@@ -11,13 +11,13 @@ public class second {
 		x = x / 3;
 		System.out.println(x);
 		
-//		課題２
+		// 課題２
 		x = 19;
 		int y = 23;
 		int z = x * y;
 		System.out.println(z);
 		
-//		課題３
+		// 課題３
 		String s = "羽柴";
 		x = 29;
 		System.out.println(s + "は" + x + "歳です。");
