8929289d1e0c0ac476c30e98162d2db7cedce7e9
m.hashiba <118049319+mizukichi0314@users.noreply.github.com>
2023-10-25 22:42:55 +0900

    first commit

---
+++ test/src/test/second.java
@@ -0,0 +1,28 @@
+package test;
+
+public class second {
+
+	public static void main(String[] args) {
+//		課題１
+		int x = 7;
+		x = x * 3;
+		System.out.println(x);
+		
+		x = x / 3;
+		System.out.println(x);
+		
+//		課題２
+		x = 19;
+		int y = 23;
+		int z = x * y;
+		System.out.println(z);
+		
+//		課題３
+		String s = "羽柴";
+		x = 29;
+		System.out.println(s + "は" + x + "歳です。");
+		
+
+	}
+
+}
