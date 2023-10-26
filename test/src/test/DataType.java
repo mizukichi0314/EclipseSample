393044c52f5684be5f08a499aae370bf78ad8024
m.hashiba <118049319+mizukichi0314@users.noreply.github.com>
2023-10-26 21:48:48 +0900

    課題：変数の代入と計算

---
+++ test/src/test/DataType.java
@@ -0,0 +1,23 @@
+package test;
+
+public class DataType {
+
+	public static void main(String[] args) {
+		
+		int month = 3;
+		int day = 14;
+		double height = 168.0;
+		float weight = 68.0f;
+		char bloodtype = 'A';
+		
+		String name = "菅原";
+		
+		System.out.println("こんにちは！" + name + "です。");
+		// 体重は架空
+		System.out.println("身長は" + height + "cm,体重は" + weight + "kgです。");
+		System.out.println("血液型は" + bloodtype + "型です。");
+		System.out.println("チャンネル登録や拡散よろしくお願いいたします！");
+		
+	}
+	
+}
--- test/src/test/test.java
+++ test/src/test/test.java
@@ -1,14 +1,15 @@
 package test;
 
-public class second {
+public class test {
 
 	public static void main(String[] args) {
+		
 		// 課題１
 		int x = 7;
 		x = x * 3;
 		System.out.println(x);
 		
-		x = x / 3;
+		x = x / 2;
 		System.out.println(x);
 		
 		// 課題２
@@ -17,11 +18,10 @@ public class second {
 		int z = x * y;
 		System.out.println(z);
 		
-		// 課題３
+		//課題３
 		String s = "羽柴";
-		x = 29;
-		System.out.println(s + "は" + x + "歳です。");
-		
+		 x = 29;
+		 System.out.println(s + "は" + x + "歳です。");
 
 	}
 
