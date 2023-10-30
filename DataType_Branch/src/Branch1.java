7fb421be059837afc438c222a7a139d1d63437cc
m.hashiba <118049319+mizukichi0314@users.noreply.github.com>
2023-10-30 22:02:56 +0900

    データ入力と条件分岐

--- DataType_Branch/src/Branch1.java
+++ DataType_Branch/src/Branch1.java
@@ -1,20 +1,36 @@
 
+// int price = Integer.parseInt(args[0]);
+		// double rate = 0.10;
+		// int discount, amount;
+		
+		// if( price >= 3000 ) {
+			// discount = 300;
+		// } else {
+			// discount = 0;
+		// }
+		
+		// amount = (int)((price - discount) * (1 + rate));
+		// System.out.println("値引金額：" + discount + "円");
+		// System.out.println("税込金額：" + amount + "円");
 public class Branch1 {
 
 	public static void main(String[] args) {
+		//上記サンプコードをif~else文に書き直した場合	
 		int price = Integer.parseInt(args[0]);
 		double rate = 0.10;
 		int discount, amount;
 		
-		if( price >= 3000 ) {
+		if( price >= 5000 ) {
+			discount = 500;
+		} else if ( price >= 3000 ) {
 			discount = 300;
 		} else {
 			discount = 0;
+			
+			amount = (int)(( price - discount ) * ( 1 + rate ));
+			System.out.println("値引金額：" + discount + "円");
+			System.out.println("税込金額：" + amount + "円");
 		}
-		
-		amount = (int)((price - discount) * (1 + rate));
-		System.out.println("値引金額：" + discount + "円");
-		System.out.println("税込金額：" + amount + "円");
 
 	}
 
