33fd4c22526f18d84e11b7055fe4e9db6f9f2b04
m.hashiba <118049319+mizukichi0314@users.noreply.github.com>
2023-11-20 20:25:41 +0900

    サンプルコード4

---
+++ array/src/array/Loop.java
@@ -0,0 +1,26 @@
+package array;
+
+public class Loop {
+
+	public static void main(String[] args) {
+		
+		// while文サンプルコード
+		// int iを初期化
+		int i = 0;
+		// iの値を３回繰り返す処理
+		while ( i < 3 ) {
+			System.out.println("i = " + i);
+			// 変数のカウントアップ
+			i++;
+		}
+		
+		// for文サンプルコード
+		/*
+		for ( int  = 0; i < 3; i++ ) {
+			System.out.println("i = " + i);
+		}
+		*/
+		
+	}
+
+}
